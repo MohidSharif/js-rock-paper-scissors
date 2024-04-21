cdf3226d6a4124daaa23902f1887b3d5cf12d0c5
Mohid Sharif <mohidtheshariff27@gmail.com>
2024-04-21 17:26:56 -0400

    added rounds, game now plays for 5 rounds

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let human_score = 0;
+let computer_score = 0;
+
+for (let i = 0; i < 5; i++){
+    let human_choice = getHumanChoice();
+    let computer_choice = getComputerChoice();
+    play_game(human_choice, computer_choice);
+    console.log("Your score: " + human_score + " Computer score: " + computer_score);
+}
 
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3)
@@ -16,29 +25,48 @@ function getHumanChoice(){
     return choice;
 }
 
-let human_choice = getHumanChoice();
-let computer_choice = getComputerChoice();
+function play_game (human_choice, computer_choice){
 
-console.log("You chose " + human_choice + " and computer chose " + computer_choice);
+    console.log("You chose " + human_choice + " and computer chose " + computer_choice);
 
-if ((human_choice == "rock") && (computer_choice == "paper")){
-    console.log("You lost!")
+    if ((human_choice == "rock") && (computer_choice == "paper")){
+        console.log("You lost!")
+        add_score("computer")
+    }
+    else if ((human_choice == "rock") && (computer_choice == "scissors")){
+        console.log("You won!")
+        add_score("human")
+    }
+    else if ((human_choice == "paper") && (computer_choice == "scissors")){
+        console.log("You lost!")
+        add_score("computer")
+    }
+    else if ((human_choice == "paper") && (computer_choice == "rock")){
+        console.log("You won!")
+        add_score("human")
+    }
+    else if ((human_choice == "scissors") && (computer_choice == "rock")){
+        console.log("You lost!")
+        add_score("computer")
+    }
+    else if ((human_choice == "scissors") && (computer_choice == "paper")){
+        console.log("You won!")
+        add_score("human")
+    }
+    else {
+        console.log("Its a tie!")
+    }
 }
-else if ((human_choice == "rock") && (computer_choice == "scissors")){
-    console.log("You won!")
+
+function add_score (winner){
+    if (winner == "human"){
+        human_score += 1;
+    }
+    else{
+        computer_score += 1;
+    }
 }
-else if ((human_choice == "paper") && (computer_choice == "scissors")){
-    console.log("You lost!")
-}
-else if ((human_choice == "paper") && (computer_choice == "rock")){
-    console.log("You won!")
-}
-else if ((human_choice == "scissors") && (computer_choice == "rock")){
-    console.log("You lost!")
-}
-else if ((human_choice == "scissors") && (computer_choice == "paper")){
-    console.log("You won!")
-}
-else {
-    console.log("Its a tie!")
-}
+
+
+
+
